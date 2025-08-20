cd9bbb54dcc9d85154dc024884c6dbe799a52182
Shreyagupta <shrutirajesh2402@gmail.com>
2025-08-20 20:53:49 +0530

    fixed errors

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,53 +1,53 @@
-// "use client"
+"use client"
 
-// import axios from "axios"
-// import Link from "next/link"
-// import { useEffect, useState } from "react"
+import axios from "axios"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
 
-// export default function VerifyEmailPage(){
-//     const[token,setToken] = useState("")
-//     const[verified,setVerified] = useState(false);
-//     const[error,setError] = useState(false);
+export default function VerifyEmailPage(){
+    const[token,setToken] = useState("")
+    const[verified,setVerified] = useState(false);
+    const[error,setError] = useState(false);
 
-//     const VerifyUserEmail = async() =>{
-//         try {
-//             await axios.post("/api/users/nodemailer",{token})
-//             setVerified(true);
-//         } catch (error) {
-//             setError(true);
-//             console.log(error)
-//         }
-//     }
-//     useEffect(() =>{
-//         const urlToken = window.location.search.split("=")[1];
-//         setToken(urlToken);
-//     } , [])
-//     useEffect(()=>{
-//         if(token.length > 0){
-//             verifyUserEmail();
-//         }
-//     },[token])
+    const verifyUserEmail = async() =>{
+        try {
+            await axios.post("/api/users/nodemailer",{token})
+            setVerified(true);
+        } catch (error) {
+            setError(true);
+            console.log(error)
+        }
+    }
+    useEffect(() =>{
+        const urlToken = window.location.search.split("=")[1];
+        setToken(urlToken);
+    } , [])
+    useEffect(()=>{
+        if(token.length > 0){
+            verifyUserEmail();
+        }
+    },[token])
 
-//     return (
-//         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-//             <h1 className="text-4xl">Verify Email</h1>
-//             <h2 className="p-2 bg-yellow-600 text-black"> {token ? `${token}` : "no token"}</h2>
-//             {
-//                 verified && (
-//                     <div> 
-//                         <h2 className="text-2xl" > Email Verified</h2>
-//                         <Link href="/login" > Login </Link>
-//                     </div>
-//                 )
-//             }
-//             {
-//                 error && (
-//                     <div> 
-//                         <h2 className="text-2xl text-red-600" >Error</h2>
-//                     </div>
-//                 )
-//             }
-//         </div>
-//     )
-//  }
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl">Verify Email</h1>
+            <h2 className="p-2 bg-yellow-600 text-black"> {token ? `${token}` : "no token"}</h2>
+            {
+                verified && (
+                    <div> 
+                        <h2 className="text-2xl" > Email Verified</h2>
+                        <Link href="/login" > Login </Link>
+                    </div>
+                )
+            }
+            {
+                error && (
+                    <div> 
+                        <h2 className="text-2xl text-red-600" >Error</h2>
+                    </div>
+                )
+            }
+        </div>
+    )
+ }
