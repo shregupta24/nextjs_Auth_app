e62a39925ef82620c4ac41ba9fac58acbc2539d2
Shreyagupta <shrutirajesh2402@gmail.com>
2025-08-20 19:52:47 +0530

    improved code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios"
 import toast from "react-hot-toast";
 
-export default function loginPage(){
+export default function LoginPage(){
     const router = useRouter();
     const [user,setUser] = useState({
         email :"",
